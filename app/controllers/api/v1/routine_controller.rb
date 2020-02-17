96590db1870cf10027daa94cd451d644860bc7a6
github username <github email address>
2020-02-16 16:23:10 -0800

    routine controller

---
+++ app/controllers/api/v1/routine_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::RoutineController < ApplicationController
+    def index
+        @routines = routine.all
+        render json: @routine
+    end
+
+    def show
+        @routine = Routine.find_by(id: params[:id])
+    end
+
+    private
+    def routine_params
+        params.require(:routine).permit(:name)
+    end
+end
---
+++ test/controllers/api/v1/routine_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::V1::RoutineControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
